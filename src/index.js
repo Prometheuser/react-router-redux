be530ba8540d0c3521a7f4b0b6dc0ff6cedd2f20
Tengchiayüeh <prometheuser.d@gmail.com>
2016-06-06 16:20:57 +0800

    refactor for ie8

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 export syncHistoryWithStore from './sync'
-export { LOCATION_CHANGE, routerReducer } from './reducer'
 
-export {
+// export { LOCATION_CHANGE, routerReducer } from './reducer'
+
+import { LOCATION_CHANGE, routerReducer } from './reducer';
+
+module.exports = LOCATION_CHANGE;
+module.exports = routerReducer;
+
+// export {
+//   CALL_HISTORY_METHOD,
+//   push, replace, go, goBack, goForward,
+//   routerActions
+// } from './actions';
+
+import {
   CALL_HISTORY_METHOD,
   push, replace, go, goBack, goForward,
   routerActions
-} from './actions'
-export routerMiddleware from './middleware'
+} from './actions';
+
+
+module.exports = CALL_HISTORY_METHOD;
+module.exports = push;
+module.exports = replace;
+module.exports = go;
+module.exports = goBack;
+module.exports = goForward;
+module.exports = routerActions;
